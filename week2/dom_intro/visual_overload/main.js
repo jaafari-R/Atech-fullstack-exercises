b971ab71fc6191b3a25745af0366a17e8a22a55a
jaafari-R <jaafariramadan@gmail.com>
2023-11-22 16:46:09 +0200

    Finished Visual Overload - Dom Intro

---
+++ week2/dom_intro/visual_overload/main.js
@@ -0,0 +1,35 @@
+const NUM_OF_BOXES_IN_ROW = 4;
+const NUM_OF_BOXES_IN_COLUMN = 4;
+const NUM_OF_BOXES = NUM_OF_BOXES_IN_ROW * NUM_OF_BOXES_IN_COLUMN;
+const GAP_BETWEEN_BOXES = 1; // %
+// value is in % units
+// const BOX_WIDTH = (100 - (NUM_OF_BOXES_IN_ROW / GAP_BETWEEN_BOXES)) / NUM_OF_BOXES_IN_ROW;
+// const BOX_HEIGHT = (100 - (NUM_OF_BOXES_IN_COLUMN / GAP_BETWEEN_BOXES)) / NUM_OF_BOXES_IN_COLUMN;
+
+const setupContainerRows= () => {
+    const container = document.getElementById('container');
+    container.style.gridTemplateColumns = `repeat(${NUM_OF_BOXES_IN_COLUMN}, 1fr)`;
+}
+
+const createBox = (id) => {
+    const container = document.getElementById('container');
+    
+    const box = document.createElement('div');
+    box.setAttribute("id", `box${id}`);
+    box.style.backgroundColor = "yellow"
+    box.onmouseover = () => {
+        const randomColor = Math.floor(Math.random()*255*255*255).toString(16);
+        box.style.backgroundColor = `#${randomColor}`;
+    }
+
+    container.appendChild(box);
+}
+
+const main = () => {
+    setupContainerRows();
+    for(let i = 0; i < NUM_OF_BOXES; ++i) {
+        createBox(i);
+    }
+}
+
+main();
