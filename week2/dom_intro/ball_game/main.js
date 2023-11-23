a10ae91ef9f9eaf345f41bfcf40468871ea0bfd5
jaafari-R <jaafariramadan@gmail.com>
2023-11-23 13:47:48 +0200

    Cleaner Ball Game code

--- week2/dom_intro/ball_game/main.js
+++ week2/dom_intro/ball_game/main.js
@@ -1,61 +1,69 @@
 const MOVE_AMMOUNT = 25;
+const MOVE_UNIT = "px";
+const INIT_EDGES = {
+    left: 0,
+    top: 0
+};
+// Each move has a corrosponding element with it's id matching a key.
+const MOVES = {
+    'right': {
+        edge: "left",
+        modifier: 1
+    },
+    'left': {
+        edge: "left",
+        modifier: -1
+    },
+    'up': {
+        edge: "top",
+        modifier: -1
+    },
+    'down': {
+        edge: "top",
+        modifier: 1
+    }
+};
 
 const ball = document.getElementById('ball');
 ball.style.backgroundColor = 'yellow';
 
-const moveBallLeft = () => {
-    const leftVal = parseInt(ball.style.left) | 0;
-    const newLeftVal = (leftVal - MOVE_AMMOUNT) + "px";
-    ball.style.left = newLeftVal;
+const moveBall = (move) => {
+    const currPos = parseInt(ball.style[move.edge]) || INIT_EDGES[move.edge];
+    ball.style[move.edge] = `${currPos + MOVE_AMMOUNT * move.modifier}${MOVE_UNIT}`;
 }
 
-const moveBallRight = () => {
-    const leftVal = parseInt(ball.style.left) | 0;
-    const newLeftVal = (leftVal + MOVE_AMMOUNT) + "px";
-    ball.style.left = newLeftVal;
+for(let key of Object.keys(MOVES)) {
+    document.getElementById(key).addEventListener("click", () => { moveBall(MOVES[key]) });
 }
 
-const moveBallUp = () => {
-    const topVal = parseInt(ball.style.top) | 0;
-    const newLeftVal = (topVal - MOVE_AMMOUNT) + "px";
-    ball.style.top = newLeftVal;
-}
-
-const moveBallDown = () => {
-    const topVal = parseInt(ball.style.top) | 0;
-    const newLeftVal = (topVal + MOVE_AMMOUNT) + "px";
-    ball.style.top = newLeftVal;
-}
-
-ball.addEventListener("keydown", (event) => {
-    const pressedKey = event.key;
-    switch(pressedKey) {
+window.addEventListener("keydown", (event) => {
+    switch(event.key) {
         case "ArrowLeft":
-            moveBallLeft();
+            moveBall(MOVES.left);
             break;
         case "ArrowRight":
-            moveBallRight();
+            moveBall(MOVES.right);
             break;
         case "ArrowUp":
-            moveBallUp();
+            moveBall(MOVES.up);
             break;
         case "ArrowDown":
-            moveBallDown();
+            moveBall(MOVES.down);
             break;
     }
 });
 
 
 const header = document.createElement("h1")
-header.innerHTML = "The Best Game Ever2"
+header.innerHTML = "The Best Game Ever"
 header.style.color = "#c0392b"
 header.style.fontFamily = "Helvetica"
 header.setAttribute("class", "my-header")
 document.body.appendChild(header)
 
-const creator = document.createElement("h2");
-creator.innerHTML = "Created by Ramadan Jaafari";
-creator.setAttribute("class", "creator-header")
-document.body.append(creator);
+const creatorHeader = document.createElement("h2");
+creatorHeader.innerHTML = "Created by Ramadan Jaafari";
+creatorHeader.setAttribute("class", "creator-header")
+document.body.append(creatorHeader);
 
 
