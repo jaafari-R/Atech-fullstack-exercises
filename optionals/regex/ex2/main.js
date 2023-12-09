f1941a1fe39da74cdd56af565dc7660ff5755f7e
jaafari-R <jaafariramadan@gmail.com>
2023-12-09 14:49:25 +0200

    Added some tests to regex exercise 2

--- optionals/regex/ex2/main.js
+++ optionals/regex/ex2/main.js
@@ -1,10 +1,27 @@
-regexArr = [/at/,/a/,/a$/,/^b/]
-
-console.log(SearchMatchingRegex('cat')) //will return true
-console.log(SearchMatchingRegex('bike')) //will return true
-console.log(SearchMatchingRegex('mouse')) //will return false 
 
 
-function SearchMatchingRegex(str) {
+// tests 1
+let regexArr = [/at/,/a/,/a$/,/^b/]
+
+console.log(searchMatchingRegex('cat')) //will return true
+console.log(searchMatchingRegex('bike')) //will return true
+console.log(searchMatchingRegex('mouse')) //will return false 
+
+
+// tests 2
+regexArr = [/a/, /b/, /^d/, /e$/];
+const str = "Bike"
+const str1 = "the room is on fire"
+const str2 = "Fergalicious"
+const str3 = "Fox in sheep clothing"
+
+
+console.log(searchMatchingRegex(str)) //return true (contains b)
+console.log(searchMatchingRegex(str1)) //return true (ends with e)
+console.log(searchMatchingRegex(str2)) //return true (contains a)
+console.log(searchMatchingRegex(str3)) //return false
+
+
+function searchMatchingRegex(str) {
     return regexArr.find(regExp => regExp.test(str)) !== undefined;
 }
