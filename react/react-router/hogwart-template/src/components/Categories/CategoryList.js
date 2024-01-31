4757c3df4b285feb04e163804d4e43ad1e439c21
jaafari-R <jaafariramadan@gmail.com>
2024-01-31 11:47:20 +0200

    improved react-router exercise code

--- react/react-router/hogwart-template/src/components/Categories/CategoryList.js
+++ react/react-router/hogwart-template/src/components/Categories/CategoryList.js
@@ -3,21 +3,16 @@ import { Link } from 'react-router-dom';
 import CategoryCard from './CategoryCard';
 
 const CategoryList = ({ categories }) => {
-  return (
-    <div className="category-list">
-        {/* add link to each of these */}
-        <Link to="/wiki/potions/" >
-          <CategoryCard id="potions" categoryData={categories[0]} />
-        </Link>
-        <Link to="/wiki/charms/" >
-          <CategoryCard id="charms" categoryData={categories[1]} />
-        </Link>
-
-      {categories.slice(2).map(category => (
-          <CategoryCard categoryData={category} />
-        ))}
-    </div>
-  );
+    console.log(categories)
+    return (
+        <div className="category-list">
+            {categories.map(category =>
+                <Link to={`/wiki/${category.name.toLowerCase()}/`} >
+                    <CategoryCard id={`${category.name.toLowerCase()}`} categoryData={category} />
+                </Link>
+            )}
+        </div>
+    );
 }
 
 export default CategoryList;
